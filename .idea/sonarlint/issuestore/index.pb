cfd6325e6f7b3449088d105a722873f5ee41c4f3
gteijema <g.teijema@afterpay.nl>
2019-12-10 13:43:34 +0100

    refactor

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+9
+	segmet.js,1/0/10efea743b3e6e2ab9894fdc74b836ead0d7ec52
+G
+seven_segment_number.js,e/1/e1ae3630cb1cff7a841d6c52d8e58cfb1dc27fb0
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+:
+
+index.html,f/6/f6013a00b362253c64368d6eebc50ea2131754e2
--- seven_segment_clock.js
+++ seven_segment_clock.js
@@ -1,31 +1,23 @@
 /*
 * Seven Digit Segments clock 
 */
-let s1;
-let s2;
-let s3;
-let s4;
-let s5;
-let s6;
+segments = [];
 
 function setup() {
   createCanvas(1300, 400);
 
-  s1 = new Segment(0);
-  s2 = new Segment(1);
-  s3 = new Segment(2);
-  s4 = new Segment(3);
-  s5 = new Segment(4);
-  s6 = new Segment(5);
+  segments.push(new Segment(0));
+  segments.push(new Segment(1));
+  segments.push(new Segment(2));
+  segments.push(new Segment(3));
+  segments.push(new Segment(4));
+  segments.push(new Segment(5));
 }
 
 function draw() {
   background(0);
 
-  s1.sevenSegments();
-  s2.sevenSegments();
-  s3.sevenSegments();
-  s4.sevenSegments();
-  s5.sevenSegments();
-  s6.sevenSegments();
+  for(let i = 0; i < segments.length; i++) {
+    segments[i].sevenSegments();	
+  }
 }
